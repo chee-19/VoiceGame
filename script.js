0a260aa663387ac78b7bb66ca93df55c738be54d
Chee Wei <cheewei21@gmail.com>
2025-09-04 13:57:25 +0800

    Allowed user to use space and enter key to change the icon

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const record = document.getElementById("record");
 const changeIcon = document.getElementById("changeIcon");
 
-record.addEventListener("click", ()=>{
+function switchIcon() {
     changeIcon.classList.toggle("fa-microphone");
     changeIcon.classList.toggle("fa-square");
+};
+
+record.addEventListener("click", ()=>{
+    switchIcon()
+});
+
+document.addEventListener("keypress", (event) => {
+    if (event.key === "Enter" || event.key ===" "){
+        switchIcon()
+    }
 });
